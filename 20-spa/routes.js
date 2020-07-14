ac20540307933241bd03699cc57497a7af9ca5f5
dhstudents <dhgithub@gmail.com>
2020-07-14 22:08:56 +0300

    fix local script and style

--- 20-spa/routes.js
+++ 20-spa/routes.js
@@ -9,16 +9,21 @@ function home(outlet) {
 function products(outlet) {
     // alert("PROD")
     outlet.innerHTML = "<h1>PRODUCTS</h1>"
+    loadScript('./components/products/product.component.js')
+    loadStyle('./components/products/product.component.css')
+    loadContent('./components/products/product.component.html',outlet)
 }
 function about(outlet) {
     // alert("ABOUT")
+    loadScript('./components/about/about.component.js')
+    loadStyle('./components/about/about.component.css')
     outlet.innerHTML = "<h1>ABOUT</h1>"
     loadContent('./components/about/about.component.html', outlet)
+   
     // fetch('./components/about/about.component.html')
     //     .then(response => response.text())
     //     .then(content => outlet.innerHTML += content)
 
-    loadScript('./components/about/about.component.js')
     // var scriptTag = document.createElement('script');
     // scriptTag.src = './components/about/about.component.js';
 
@@ -33,10 +38,47 @@ function loadContent(url, outlet) {
         .then(content => outlet.innerHTML += content)    
 }
 
+
 function loadScript(url) {
-    var scriptTag = document.createElement('script');
+    const oldScript = document.querySelector('#dynamicScript')
+    oldScript?.remove();
+    const scriptTag = document.createElement('script');
+    scriptTag.id = "dynamicScript"
     scriptTag.src = url;
-    document.body.insertAdjacentElement('beforeend',scriptTag)   
+    document.body.appendChild(scriptTag)   
+    // document.body.insertAdjacentElement('beforeend',scriptTag)   
 }
 
-export { home , products , about }
+function loadStyle(url) {
+    const oldScript = document.querySelector('#dynamicStylesheet')
+    oldScript?.remove();
+    let styleTag= document.createElement("link")
+    styleTag.setAttribute("rel", "stylesheet")
+    styleTag.setAttribute("id", "dynamicStylesheet")
+    styleTag.setAttribute("type", "text/css")
+    styleTag.setAttribute("href", url) 
+    document.head.appendChild(styleTag)   
+     
+}
+
+
+
+export { home, products, about }
+
+
+
+// function loadjscssfile(filename, filetype){
+//     if (filetype=="js"){ //if filename is a external JavaScript file
+//      var fileref=document.createElement('script')
+//      fileref.setAttribute("type","text/javascript")
+//      fileref.setAttribute("src", filename)
+//     }
+//     else if (filetype=="css"){ //if filename is an external CSS file
+//      var fileref=document.createElement("link")
+//      fileref.setAttribute("rel", "stylesheet")
+//      fileref.setAttribute("type", "text/css")
+//      fileref.setAttribute("href", filename)
+//     }
+//     if (typeof fileref!="undefined")
+//      document.getElementsByTagName("head")[0].appendChild(fileref)
+//    }
